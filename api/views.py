c904635c42418e60629d73b28af6df901cd94d24
Luan Vieira <luanvlw23@gmail.com>
2025-09-08 20:35:09 -0300

    Adicionar página inicial e rota raiz

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from .models import Produto
 from .serializers import ProdutoSerializer
+from django.http import HttpResponse
 
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+
+def home(request):
+    return HttpResponse("Página inicial de tarefas 🚀")
