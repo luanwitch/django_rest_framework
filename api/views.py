92714ad3e3db65bc9463fc4a0823910524eca567
Luan Vieira <luanvwittchind@gmail.com>
2025-09-29 15:05:55 -0300

    Corrigindo view home e templates

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from .models import Produto
 from .serializers import ProdutoSerializer
 from django.http import HttpResponse
+from api.views import home
+
 
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all()
